116e3b55ab6f1902a2c9e7dd587a88a0d61801ec
vmuhembe <vmuhembe@gmail.com>
2024-07-13 11:15:05 +0300

    added the html templates and modified the main file to read the content of the templates

--- main.go
+++ main.go
@@ -6,33 +6,81 @@ import (
 	"net/http"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-
-	tmplt, err := template.ParseFiles("templates/index.html")
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	tmplt.Execute(w, nil)
-
+type Rsvp struct {
+	Name, Email, Phone string
+	WillAttend         bool
 }
 
-func HandleContact(w http.ResponseWriter, r *http.Request) {
+var responses = make([]*Rsvp, 0, 10)
+var templates = make(map[string]*template.Template, 3)
 
-	tmplt, err := template.ParseFiles("templates/contact.html")
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+func loadTemplates() {
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for _, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+		} else {
+			panic(err)
+		}
 	}
-	tmplt.Execute(w, nil)
-
 }
-
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, nil)
+}
+func listHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["list"].Execute(writer, responses)
+}
 func main() {
+	loadTemplates()
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/list", listHandler)
+	http.HandleFunc("/form", formHandler)
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
-	http.HandleFunc("/", Handler)
-	http.HandleFunc("/contact", HandleContact)
-	http.ListenAndServe(":5000", nil)
+type formData struct {
+	*Rsvp
+	Errors []string
+}
+
+func formHandler(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		templates["form"].Execute(writer, formData{
+			Rsvp: &Rsvp{}, Errors: []string{},
+		})
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+		responseData := Rsvp{
+			Name:       request.FormValue("name"),
+			Email:      request.FormValue("email"),
+			Phone:      request.FormValue("phone"),
+			WillAttend: request.FormValue("willattend") == "true",
+		}
+		errors := []string{}
+		if responseData.Name == "" {
+			errors = append(errors, "Please enter your name")
+		}
+		if responseData.Email == "" {
+			errors = append(errors, "Please enter your email address")
+		}
+		if responseData.Phone == "" {
+			errors = append(errors, "Please enter your phone number")
+		}
+		if len(errors) > 0 {
+			templates["form"].Execute(writer, formData{
+				Rsvp: &responseData, Errors: errors,
+			})
+		} else {
+			responses = append(responses, &responseData)
+			if responseData.WillAttend {
+				templates["thanks"].Execute(writer, responseData.Name)
+			} else {
+				templates["sorry"].Execute(writer, responseData.Name)
+			}
+		}
+	}
 }
